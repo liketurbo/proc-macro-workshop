ae610bf32cfe18dd9721c0c1ad490f018afa1b12
Rami Chasygov <liketurbo.work@gmail.com>
2024-07-05 20:25:24 +0300

    Pass 08-unrecognized-attribute

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,9 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{
-    parse_macro_input, punctuated::Punctuated, AngleBracketedGenericArguments, Attribute, Data,
-    DeriveInput, Expr, ExprLit, Fields, GenericArgument, Lit, Meta, MetaNameValue, PathArguments,
-    Token, Type, TypePath,
+    parse_macro_input, punctuated::Punctuated, spanned::Spanned, AngleBracketedGenericArguments,
+    Attribute, Data, DeriveInput, Expr, ExprLit, Fields, GenericArgument, Lit, Meta, MetaNameValue,
+    PathArguments, Token, Type, TypePath,
 };
 
 #[proc_macro_derive(Builder, attributes(builder))]
@@ -16,7 +16,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields = fields.iter().map(|field| {
         let name = &field.ident;
         let ty = &field.ty;
-        if is_option(ty) || find_attr_each(&field.attrs).is_some() {
+        if is_option(ty) || find_attr(&field.attrs, "each").is_some() {
             quote! {
                 #name: #ty
             }
@@ -30,7 +30,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let build_fields = fields.iter().map(|field| {
         let name = &field.ident;
         let ty = &field.ty;
-        if find_attr_each(&field.attrs).is_some() {
+        if find_attr(&field.attrs, "each").is_some() {
             quote! {
                 #name: std::mem::take(&mut self.#name)
             }
@@ -48,40 +48,55 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let build_fields_none = fields.iter().map(|field| {
         let name = &field.ident;
-        if find_attr_each(&field.attrs).is_some() {
-            quote! { 
-                #name: Vec::new() 
+        if find_attr(&field.attrs, "each").is_some() {
+            quote! {
+                #name: Vec::new()
             }
         } else {
-            quote! { 
-                #name: None 
+            quote! {
+                #name: None
             }
         }
     });
 
-    let setters = fields.iter().map(|field| {
-        let name = &field.ident;
-        let ty = &field.ty;
+    let setters: Result<Vec<_>, syn::Error> = fields
+        .iter()
+        .map(|field| {
+            let name = &field.ident;
+            let ty = &field.ty;
 
-        if let Some(each_name) = find_attr_each(&field.attrs) {
-            let each_ident = format_ident!("{each_name}");
-            let vec_inner_ty = extract_inner_ty(ty, "Vec").unwrap();
-            quote! {
-                pub fn #each_ident(&mut self, #each_ident: #vec_inner_ty) -> &mut Self {
-                    self.#name.push(#each_ident);
-                    self
+            if let Some(attr_info) = find_attr(&field.attrs, "each") {
+                match attr_info {
+                    AttributeInfo::Correct { value } => {
+                        let each_ident = format_ident!("{value}");
+                        let vec_inner_ty = extract_inner_ty(ty, "Vec").unwrap();
+                        Ok(quote! {
+                            pub fn #each_ident(&mut self, #each_ident: #vec_inner_ty) -> &mut Self {
+                                self.#name.push(#each_ident);
+                                self
+                            }
+                        })
+                    }
+                    AttributeInfo::Misspelled { span } => {
+                        Err(syn::Error::new(span, "expected `builder(each = \"...\")`"))
+                    }
                 }
+            } else {
+                let setter_ty = extract_inner_ty(ty, "Option").unwrap_or(ty);
+                Ok(quote! {
+                    pub fn #name(&mut self, #name: #setter_ty) -> &mut Self {
+                        self.#name = Some(#name);
+                        self
+                    }
+                })
             }
-        } else {
-            let setter_ty = extract_inner_ty(ty, "Option").unwrap_or(ty);
-            quote! {
-                pub fn #name(&mut self, #name: #setter_ty) -> &mut Self {
-                    self.#name = Some(#name);
-                    self
-                }
-            }
-        }
-    });
+        })
+        .collect();
+
+    let setters = match setters {
+        Ok(s) => s,
+        Err(e) => return e.into_compile_error().into(),
+    };
 
     let tokens = quote! {
         struct #builder_name {
@@ -145,19 +160,33 @@ fn extract_inner_ty<'a>(ty: &'a Type, container: &str) -> Option<&'a Type> {
     None
 }
 
-fn find_attr_each(attrs: &[Attribute]) -> Option<String> {
+const LEVENSHTEIN_THRESHOLD: usize = 1;
+
+enum AttributeInfo {
+    Correct { value: String },
+    Misspelled { span: proc_macro2::Span },
+}
+
+fn find_attr<'a>(attrs: &'a [Attribute], attr_key: &str) -> Option<AttributeInfo> {
     for attr in attrs {
+        let span = attr.meta.span();
         if attr.path().is_ident("builder") {
             let nested = attr
                 .parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)
                 .expect("attributes parsing failed");
             for meta in nested {
                 if let Meta::NameValue(MetaNameValue { path, value, .. }) = meta {
-                    // #[builder(each = "arg")]
-                    if path.is_ident("each") {
-                        if let Expr::Lit(ExprLit { lit, .. }) = value {
-                            if let Lit::Str(lit_str) = lit {
-                                return Some(lit_str.value());
+                    if let Expr::Lit(ExprLit { lit, .. }) = value {
+                        if let Lit::Str(lit_str) = lit {
+                            let cur_attr_key = path.get_ident().unwrap().to_string();
+                            let attr_value = lit_str.value();
+                            let dist = levenshtein_distance(&cur_attr_key, attr_key);
+                            if dist == 0 {
+                                return Some(AttributeInfo::Correct { value: attr_value });
+                            } else if dist <= LEVENSHTEIN_THRESHOLD {
+                                return Some(AttributeInfo::Misspelled { span });
+                            } else {
+                                return None;
                             }
                         }
                     }
@@ -166,4 +195,35 @@ fn find_attr_each(attrs: &[Attribute]) -> Option<String> {
         }
     }
     None
+}
+
+fn levenshtein_distance(s1: &str, s2: &str) -> usize {
+    let len1 = s1.len();
+    let len2 = s2.len();
+    let mut matrix = vec![vec![0; len2 + 1]; len1 + 1];
+
+    for i in 0..=len1 {
+        matrix[i][0] = i;
+    }
+
+    for j in 0..=len2 {
+        matrix[0][j] = j;
+    }
+
+    for i in 1..=len1 {
+        for j in 1..=len2 {
+            let cost = if s1.chars().nth(i - 1) == s2.chars().nth(j - 1) {
+                0
+            } else {
+                1
+            };
+
+            matrix[i][j] = std::cmp::min(
+                std::cmp::min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1),
+                matrix[i - 1][j - 1] + cost,
+            );
+        }
+    }
+
+    matrix[len1][len2]
 }
